1255b235d13d6f1a4f2b24262a3180f4589505b8
Kenneth Aurelio <kennethaurelio24@gmail.com>
2026-08-21 21:57:12 +0800

    Fix project references after renaming to LibrarySystem.Models

--- LibraryManagementSystem/LibrarySystem.Models/obj/Release/net10.0-windows/LibrarySystem.Models.AssemblyInfo.cs
+++ LibraryManagementSystem/LibrarySystem.Models/obj/Release/net10.0-windows/LibrarySystem.Models.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibrarySystem.Models")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4cf4353d725e320f1d770c8c0fc87dc09b3d7e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibrarySystem.Models")]
