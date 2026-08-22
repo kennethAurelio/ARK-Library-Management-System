424791b708df1ae585fc9b7309b9b09348e5cbb3
Kenneth Aurelio <kennethaurelio24@gmail.com>
2026-08-22 11:04:34 +0800

    Add .gitignore to ignore .vs, bin, obj

--- LibraryManagementSystem/LibrarySystem.Models/obj/Debug/net10.0-windows/LibrarySystem.Models.AssemblyInfo.cs
+++ LibraryManagementSystem/LibrarySystem.Models/obj/Debug/net10.0-windows/LibrarySystem.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibrarySystem.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+164d36861ad1062c31c924052b1510a21a732594")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a06d9d026f43a79a33bef2267bf193b7f8c7b065")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibrarySystem.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibrarySystem.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
