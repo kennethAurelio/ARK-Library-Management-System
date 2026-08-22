31238fee1f82dc198dc76c7c034a48830891af83
Kenneth Aurelio <kennethaurelio24@gmail.com>
2026-08-22 11:15:21 +0800

    Cleanup: remove .vs and build artifacts

--- LibraryManagementSystem/LibrarySystem.Models/obj/Debug/net10.0-windows/LibrarySystem.Models.AssemblyInfo.cs
+++ LibraryManagementSystem/LibrarySystem.Models/obj/Debug/net10.0-windows/LibrarySystem.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibrarySystem.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1255b235d13d6f1a4f2b24262a3180f4589505b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+296d796764ee3cc583f2d3ae278034bae594bb60")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibrarySystem.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibrarySystem.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
